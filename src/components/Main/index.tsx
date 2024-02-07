34f3d8fe01ff2c9d8902609f44789ac166171f76
Maria Luiza Stellet <maria.stellet@olxbr.com>
2024-02-07 18:42:15 -0300

    config: storybook

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,11 +1,21 @@
 import * as S from './styles'
 
-function Main() {
-  return (
-    <S.Wrapper>
-      <h1>main</h1>
-    </S.Wrapper>
-  )
-}
+const Main = ({
+  title = 'React Avançado',
+  description = 'TypeScript, ReactJS, NextJS e Styled Components'
+}) => (
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado."
+    />
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor de frente para uma tela com código."
+    />
+  </S.Wrapper>
+)
 
-export default Main
+export default Main;
